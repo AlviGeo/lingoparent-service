9e8c04f60aec886081a7a93f2c3cd32753f21cdc
latifis <latifirfansyah@gmail.com>
2021-10-29 10:18:39 +0700

    create user with checking role

--- src/controllers/v1/master/register.js
+++ src/controllers/v1/master/register.js
@@ -12,6 +12,7 @@ const bcrypt = require("bcrypt");
 const db = require("../../../models/v1")
 const User = db.master_user;
 const Parent = db.master_parent;
+const Students = db.master_student;
 
 // Validator //
 const Validator = require("fastest-validator");
@@ -51,12 +52,11 @@ const register = async (req, res) => {
       return res.status(409).json({
           status: 'error',
           message: 'email already exist'
-      });
-      
-  }
+      }); 
+    }
 
-  /*Validate Register Requirement*/
-  const checkRegister = v.compile(validator.register);
+    /*Validate Register Requirement*/
+    const checkRegister = v.compile(validator.register);
     const check = checkRegister(
       { 
         firstname: firstname,
@@ -98,28 +98,63 @@ const register = async (req, res) => {
     
     const UserIN = await User.create(dataUser);
     
+    /* Create DB with Checking Role */
+    switch (role){
+			case "students":
+				
+        console.log("masuk students");
+        /*Create Data Students with Find Email*/
+        const students = await User.findOne({
+          where: {
+            email: email
+          },
+        });
+
+        const dataStudents = {
+          firstname: firstname,
+          lastname: lastname,
+          phone: phone,
+          idUser_create: students.id,
+          address: address,
+          gender: gender,
+          date_birth: date_birth,
+          photo: photo
+        };
+        const StudentsIN = await Students.create(dataStudents);
+
+        return successResponse(req, res, {UserIN, StudentsIN});
+
+			case "parent":
+        
+        console.log("masuk parent");
+				/*Create Data Parent with Find Email*/
+        const parent = await User.findOne({
+          where: {
+            email: email
+          },
+        });
+
+        const dataParent = {
+          firstname: firstname,
+          lastname: lastname,
+          phone: phone,
+          idUser_create: parent.id,
+          address: address,
+          gender: gender,
+          date_birth: date_birth,
+          photo: photo
+        };
+        const ParentIN = await Parent.create(dataParent);
+
+        return successResponse(req, res, {UserIN, ParentIN});
+
+			default:
+			  return res.status(409).json({
+          status: 'error',
+          message: "role dosen\'t exist"
+      });
+		}
     
-    /*Create Data Parent with Find Email*/
-    const parent = await User.findOne({
-      where: {
-        email: email
-      },
-    });
-
-    const dataParent = {
-      firstname: firstname,
-      lastname: lastname,
-      phone: phone,
-      idUser_create: parent.id,
-      address: address,
-      gender: gender,
-      date_birth: date_birth,
-      photo: photo
-    };
-    const ParentIN = await Parent.create(dataParent);
-
-    return successResponse(req, res, {UserIN, ParentIN});
-
   } catch (err) {
     return errorResponse(req, res, {
       err
